fffb52880d3936e4bf54f3cdef75458cad7ac9c1
wheeliez <mathieu.dargis@gmail.com>
2013-12-13 21:53:32 -0500

    Modif fct creer vente

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 
 public class Main {
 
@@ -10,18 +12,20 @@ public class Main {
 		RegistreClients clients = RegistreClients.getInstance();
 		RegistreFactures factures = RegistreFactures.getInstance();
 		
-		Article article = catalogue.find("S1");
-		Facture f = Vente.creerVente(article, 1, new Client("Frederic", "Hamelin", 1));
-		Vente.ajouterArticle(f, catalogue.find("P1"), 1);		
+		List<Article> lst = new ArrayList<Article>();
+		lst.add(catalogue.find("S1"));
+		Facture f = Vente.creerVente(lst, 1, new Client("Frederic", "Hamelin", 1));	
 		
-		Article article1 = catalogue.find("S1");
-		Vente.creerVente(article1, 1, new Client("Mathieu", "Dargis", 2));
+		lst = new ArrayList<Article>();
+		lst.add(catalogue.find("S1"));
+		Vente.creerVente(lst, 1, new Client("Mathieu", "Dargis", 2));
 		
-		Article article2 = catalogue.find("P1");
-		Vente.creerVente(article2, 1, new Client("Simon", "Blanchette", 3));
+		lst = new ArrayList<Article>();
+		lst.add(catalogue.find("P1"));
+		Vente.creerVente(lst, 1, new Client("Simon", "Blanchette", 3));
 		
-		
-		Article article3 = catalogue.find("B1");
-		Vente.creerVente(article3, 1, new Client("Christophe", "Diamond", 4));
+		lst = new ArrayList<Article>();
+		lst.add(catalogue.find("B1"));
+		Vente.creerVente(lst, 1, new Client("Christophe", "Diamond", 4));
 	}
 }
--- Vente.java
+++ Vente.java
@@ -1,15 +1,17 @@
+import java.util.List;
+
 
 public class Vente {
 	
-	public static Facture creerVente(Article _a, int _quantite, Client _client) {
+	public static Facture creerVente(List<Article> _a, int _quantite, Client _client) {
 		RegistreFactures regFac = RegistreFactures.getInstance();
 		
 		Facture f = new Facture(123123, _client);
-		regFac.add(f);
 		
-		if (f.ajouterArticle(_a, _quantite))
-		{
-			System.out.println("Yep");
+		regFac.add(f);
+			
+		for (int i = 0 ; i < _a.size(); i++) {
+			f.ajouterArticle(_a.get(i), _quantite);
 		}
 		
 		return f;
